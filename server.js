994114d418032c14103652e1652bd3dba1d8f9a0
Vincentvirtuoso <vincentvirtuoso66@gmail.com>
2025-03-20 11:18:42 +0100

    test environment -2

--- server.js
+++ server.js
@@ -10,23 +10,24 @@ import orderRouter from './routes/orderRoutes.js';
 const app = express();
 const port = process.env.PORT || 4000;
 
-const allowedOrigin = [
-  'http://localhost:5173/',
-  'https://kitchen-connect-com.onrender.com/',
-];
+// const allowedOrigin = [
+//   'http://localhost:5173/',
+//   'https://kitchen-connect-com.onrender.com/',
+// ];
 
 // middleware
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigin.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: ['GET', 'PUT', 'POST', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: '*',
+    // : function (origin, callback) {
+    //   if (!origin || allowedOrigin.includes(origin)) {
+    //     callback(null, true);
+    //   } else {
+    //     callback(new Error('Not allowed by CORS'));
+    //   }
+    // },
+    // methods: ['GET', 'PUT', 'POST', 'DELETE'],
+    // allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
 app.use(express.json());
